a50e9f051bf87e251ace0b13a099792cc8573239
terryyLi <jielinl@andrew.cmu.edu>
2023-02-10 00:13:18 -0500

    added readinglist to src/controllers

--- src/controllers/accounts/helpers.js
+++ src/controllers/accounts/helpers.js
@@ -164,6 +164,7 @@ async function getCounts(userData, callerUID) {
         promises.upvoted = db.sortedSetCard(`uid:${uid}:upvote`);
         promises.downvoted = db.sortedSetCard(`uid:${uid}:downvote`);
         promises.bookmarks = db.sortedSetCard(`uid:${uid}:bookmarks`);
+        promises.readinglists = db.sortedSetCard(`uid:${uid}:readinglists`);
         promises.uploaded = db.sortedSetCard(`uid:${uid}:uploads`);
         promises.categoriesWatched = user.getWatchedCategories(uid);
         promises.blocks = user.getUserField(userData.uid, 'blocksCount');
