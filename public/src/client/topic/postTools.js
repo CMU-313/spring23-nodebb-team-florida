8ddb2b5dc70686ab5d9b12eb88278cf0aac476c1
terryyLi <jielinl@andrew.cmu.edu>
2023-02-09 22:55:44 -0500

    added readinglist functions in postTool.js

--- public/src/client/topic/postTools.js
+++ public/src/client/topic/postTools.js
@@ -64,7 +64,7 @@ define('forum/topic/postTools', [
     PostTools.toggle = function (pid, isDeleted) {
         const postEl = components.get('post', 'pid', pid);
 
-        postEl.find('[component="post/quote"], [component="post/bookmark"], [component="post/reply"], [component="post/flag"], [component="user/chat"]')
+        postEl.find('[component="post/quote"], [component="post/bookmark"], [component="post/readinglist"], [component="post/reply"], [component="post/flag"], [component="user/chat"]')
             .toggleClass('hidden', isDeleted);
 
         postEl.find('[component="post/delete"]').toggleClass('hidden', isDeleted).parent().attr('hidden', isDeleted ? '' : null);
@@ -114,6 +114,10 @@ define('forum/topic/postTools', [
 
         postContainer.on('click', '[component="post/bookmark"]', function () {
             return bookmarkPost($(this), getData($(this), 'data-pid'));
+        });
+
+        postContainer.on('click', '[component="post/readinglist"]', function () {
+            return readinglistPost($(this), getData($(this), 'data-pid'));
         });
 
         postContainer.on('click', '[component="post/upvote"]', function () {
@@ -359,6 +363,19 @@ define('forum/topic/postTools', [
                 return alerts.error(err);
             }
             const type = method === 'put' ? 'bookmark' : 'unbookmark';
+            hooks.fire(`action:post.${type}`, { pid: pid });
+        });
+        return false;
+    }
+
+    function readinglistPost(button, pid) {
+        const method = button.attr('data-readinglisted') === 'false' ? 'put' : 'del';
+
+        api[method](`/posts/${pid}/readinglist`, undefined, function (err) {
+            if (err) {
+                return alerts.error(err);
+            }
+            const type = method === 'put' ? 'readinglist' : 'unreadinglist';
             hooks.fire(`action:post.${type}`, { pid: pid });
         });
         return false;
