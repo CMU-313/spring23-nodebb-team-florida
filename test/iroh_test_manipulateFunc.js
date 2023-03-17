cfeb09d5763628b85634a8f48d60d5446adb11cd
bmwang <bw2@andrew.cmu.edu>
2023-03-17 03:27:07 -0400

    manipulate functions with iroh

---
+++ test/iroh_test_manipulateFunc.js
@@ -0,0 +1,15 @@
+const Iroh = require('iroh');
+
+function add(a, b) {
+    return a + b;
+  };
+  
+  let stage = new Iroh.Stage(add.toString());
+  stage.addListener(Iroh.FUNCTION).on("return", function(e) {
+    if (e.name === "add") e.return = 42;
+  });
+  
+  eval(stage.script);
+  
+  console.log(add(2, 2));
+  console.log(add(10, 5));
